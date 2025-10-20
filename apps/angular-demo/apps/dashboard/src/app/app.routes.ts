9c9f73778d218fc28a00d3989019f45e2cd59daf
tankv <tankieu7c@gmail.com>
2025-10-21 00:49:54 +0700

    second commit

---
+++ apps/angular-demo/apps/dashboard/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { NxWelcome } from './nx-welcome';
+import { Route } from '@angular/router';
+import {loadRemoteModule} from "@nx/angular/mf";
+
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    loadChildren: () => loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: '',
+    component: NxWelcome,
+  },
+];
---
+++ apps/angular-demo/apps/dashboard/src/app/app.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { distinctUntilChanged } from 'rxjs/operators';
+import {UserAuth} from "@angular-demo/data-access-user";
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  selector: 'ng-mf-root',
+  template: `
+    <div class="dashboard-nav">Admin Dashboard</div>
+    <div *ngIf="isLoggedIn$ | async; else signIn">
+      You are authenticated so you can see this content.
+    </div>
+    <ng-template #signIn><router-outlet></router-outlet></ng-template>
+  `,
+})
+export class App implements OnInit {
+  private router = inject(Router);
+  private userService = inject(UserAuth);
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        // Queue the navigation after initialNavigation blocking is completed
+        setTimeout(() => {
+          if (!loggedIn) {
+            this.router.navigateByUrl('login');
+          } else {
+            this.router.navigateByUrl('');
+          }
+        });
+      });
+  }
+}
